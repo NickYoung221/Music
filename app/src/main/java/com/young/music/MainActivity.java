6bc3571f0a6aceeaa1a24da5b1d5401ae3edc930
young <1398508918@qq.com>
2016-10-26 19:36:36 +0800

    第四次提交

--- app/src/main/java/com/young/music/MainActivity.java
+++ app/src/main/java/com/young/music/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends ListActivity implements AdapterView.OnItemClic
         myApplication= (MyApplication) getApplication();
         //list保存在application中
         myApplication.setMusicInfoList(lists);
-        Log.i("MainActivity", "getData: ");
+        Log.i("MainActivity", "getData: 111");
         Log.i("MainActivity", "getData: 2");
     }
 
